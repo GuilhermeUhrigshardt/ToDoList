42e5452e228f15fa4a7a309ff4ec2201839b4443
Guilherme Guimarães Uhrigsha <guilherme.guimaraes@tce.pr.gov.br>
2024-09-19 09:02:51 -0300

    removing versioning and soft delete

--- src/ToDoList.Domain/Entities/BaseEntity.cs
+++ src/ToDoList.Domain/Entities/BaseEntity.cs
@@ -5,8 +5,5 @@ namespace ToDoList.Domain.Entities;
 public abstract class BaseEntity
 {
     protected Guid Id { get; set; }
-    protected bool Active { get; set; } = true;
-    protected int Version { get; set; } = 1;
     protected DateTime? Inserted { get; set; } = DateTime.Now;
-    protected DateTime? Updated { get; set; }
 }
