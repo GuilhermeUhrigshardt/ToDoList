e496713ba41866216576e7b5bb92eced190649f6
Guilherme Guimarães Uhrigsha <guilherme.guimaraes@tce.pr.gov.br>
2024-10-11 10:53:52 -0300

    start validation checklist

---
+++ src/ToDoList.Application/Features/Checklist/Commands/Create/CreateChecklistCommandValidator.cs
@@ -0,0 +1,9 @@
+using System;
+using FluentValidation;
+
+namespace ToDoList.Application.Features.Checklist.Commands.Create;
+
+public class CreateChecklistCommandValidator : AbstractValidator<CreateChecklistCommand>
+{
+    // TODO: Implement validation
+}
